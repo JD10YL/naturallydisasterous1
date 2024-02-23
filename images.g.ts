56f30da8fbed26f33339e0cf60db211934377788
JD10YL <155397262+JD10YL@users.noreply.github.com>
2024-02-23 12:38:45 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "C":return img`
+...........f..........
+..........faff........
+..........faaaf.......
+..........faaaf.......
+.ff.....ffaaaaf.......
+.fff....faaaaaf.......
+.faff...faaaaaf....ff.
+.faaff..faaaaaf...faf.
+.faaaff.faaaaaf...faf.
+.faaaaffaaaaaaf..faaf.
+..faaafaaaaaaaf..faaf.
+..ffaafaaaaafff.faaaf.
+...faaafaaaaf..faaaf..
+...ffbbbbbbbbbbbaaaf..
+....bffbbfbbbffbbff...
+....bfbbbbbbffbbb.....
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
